c2bdcc02868113e31f2db3928d06cbb4620919d3
scub111 <scub111@mail.ru>
2019-10-16 23:43:20 +0300

    Temp commit.

--- packages/server2/src/main.ts
+++ packages/server2/src/main.ts
@@ -1,5 +1,8 @@
 
 import { test } from '@mapbul-pub/codegen2';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from 'server/app.module';
+import { join } from 'path';
 
 export const main = async () => {
   // await sleep(500);
@@ -12,3 +15,21 @@ export const main = async () => {
 };
 
 main();
+
+async function bootstrap() {
+  test();
+  // codegenTest();
+  const port = process.env.PORT || 3200;
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setBaseViewsDir(join(__dirname, 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(port);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  console.log(`Server started at http://localhost:${port}`);
+}
+bootstrap();
