fc1ceca367dd969a2431b4b890ab58bb04efad36
scub111 <scub111@mail.ru>
2019-10-27 12:43:17 +0300

    Server3's build works.

--- packages/server3/webpack.config.js
+++ packages/server3/webpack.config.js
@@ -1,32 +1,30 @@
 const webpack = require('webpack');
 const path = require('path');
 const nodeExternals = require('webpack-node-externals');
-// const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
-  target: 'node',
-  mode: 'production',
-  entry: ['./src/main.ts'],
+  entry: ['webpack/hot/poll?100', './src/main.ts'],
   // watch: true,
+  target: 'node',
   externals: [
-    nodeExternals(),
+    nodeExternals({
+      whitelist: ['webpack/hot/poll?100'],
+    }),
   ],
   module: {
     rules: [
       {
-        test: /.tsx?$/,
+        test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
+  mode: 'development',
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
-    // plugins: [new TsconfigPathsPlugin({ /*configFile: "./path/to/tsconfig.json" */ })]
   },
-  // optimization: {
-  //   minimize: true
-  // },
+  plugins: [new webpack.HotModuleReplacementPlugin()],
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'main.js',
